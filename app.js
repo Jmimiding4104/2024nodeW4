3b9ec0806432ebf81078c4800710888dbe233f84
Jmimiding4104@gmail.com <Jmimiding4104>
2024-04-08 03:07:58 +0800

    up

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ require('dotenv').config();
 require('./connections');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
